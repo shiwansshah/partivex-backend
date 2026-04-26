af2ba0f9dfa862a9882806c18649d92aa1c32caa
Sumanta6 <sumantagautamm@gmail.com>
2026-04-26 22:44:50 +0545

    minor change

---
+++ Partivex/Controllers/VendorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Partivex.Controllers
+{
+    public class VendorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
